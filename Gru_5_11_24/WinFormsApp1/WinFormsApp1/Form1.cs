cf658b02f7dede2bacf961ea5596d9ee89d3096c
ALE1CANA <alessandro.salvi07@icloud.com>
2024-11-05 23:16:38 +0100

    Add files via upload

---
+++ Gru_5_11_24/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Gru gru;
+        StazioneDiRadiocomando stazione;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gru = new Gru(1, "Produttore", 309, 146);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Location.Y-5 > gru.HMin)
+            {
+                gru.Alza(5);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 5);
+            }
+            else
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X, gru.HMin);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Location.Y + 5 < gru.HMax)
+            {
+                gru.Abbassa(5);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 5);
+            }
+            else
+            {
+                pictureBox2.Location = new Point(pictureBox2.Location.X, gru.HMax);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, gru.HMin);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt16(textBox2.Text) >= 146 && Convert.ToInt16(textBox1.Text) <= 309)
+            gru.HMin = Convert.ToInt16(textBox2.Text);
+            gru.HMax = Convert.ToInt16(textBox1.Text);
+            pictureBox2.Location = new Point(pictureBox2.Location.X, gru.HMin);
+        }
+    }
+}
---
+++ Gru_5_11_24/WinFormsApp1/WinFormsApp1/Gru.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Gru
+    {
+        private int numSeriale;
+        private string produttore;
+        private int hMax;
+        private int hMin;
+        private int hBraccio;
+        public Gru(int numSeriale, string produttore, int hMax, int hMin)
+        {
+            this.numSeriale = numSeriale;
+            this.produttore = produttore;
+            this.hMax = hMax;
+            this.hMin = hMin;
+            this.hBraccio = hMin;
+        }
+        public int HBraccio
+        {
+            get { return hBraccio; }
+            set
+            {
+                if (value > hMax)
+                {
+                    hBraccio = hMax;
+                    Console.WriteLine("Altezza massima raggiunta.");
+                }
+                else if (value < hMin)
+                {
+                    hBraccio = hMin;
+                    Console.WriteLine("Altezza minima raggiunta.");
+                }
+                else
+                {
+                    hBraccio = value;
+                }
+            }
+        }
+        public int HMax
+        {
+            get { return hMax; }
+            set { hMax = value; }
+        }
+        public int HMin
+        {
+            get { return hMin; }
+            set { hMin = value; }
+        }
+        public int NumSeriale
+        {
+            get { return numSeriale; }
+        }
+        public string Produttore
+        {
+            get { return produttore; }
+        }
+        public void Alza(int metri)
+        {
+            HBraccio += metri;
+        }
+
+        public void Abbassa(int metri)
+        {
+            HBraccio -= metri;
+        }
+        public void RiportaInPosizioneSicurezza()
+        {
+            HBraccio = hMin;
+        }
+        public int GetAltezza()
+        {
+            return HBraccio;
+        }
+    }
+}
